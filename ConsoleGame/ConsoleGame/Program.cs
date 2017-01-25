bfe602b758b42818d316f172b15eb19bd4695c4c
Junsoo Park <parkjunsoo91@gmail.com>
2017-01-25 17:52:41 +0900

    game class created

---
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleGame;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Game x = new Game();
+
+            Console.Read();
+        }
+    }
+}
